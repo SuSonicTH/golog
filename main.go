b34c13fdab1593c0c01e67b448f58c98d4186bd5
Michael Wolf <susonicth@gmail.com>
2026-03-24 23:41:18 +0100

    using iterator; added commandline flags and atching/searching

--- main.go
+++ main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strings"
 )
 
 func main() {
-	lr := newLogReader("BlackMed.log", "^(\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d,\\d\\d\\d) \\[([A-Za-z]+)\\] ([^ ]+)")
-	for true {
-		if le := lr.next(); le == nil {
-			return
-		} else if le.class == "ERROR" {
-			fmt.Print(le.line)
+	var search string
+	var class string
+	var level string
+	var lines bool
+
+	flag.StringVar(&search, "search", "", "regular expression to match")
+	flag.StringVar(&class, "class", "", "a class to match (literal match)")
+	flag.StringVar(&level, "level", "", "a level to match (literal match)")
+	flag.BoolVar(&lines, "lines", false, "print a line to seperate messages")
+
+	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Fprint(os.Stderr, "Error: no file argument given\n")
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.CommandLine.PrintDefaults()
+		return
+	}
+
+	var searchRe *regexp.Regexp
+	if search != "" {
+		var searchReErr error
+		searchRe, searchReErr = regexp.Compile(search)
+		if searchReErr != nil {
+			panic(searchReErr)
+		}
+	}
+
+	re, err := regexp.Compile(`^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d,\d\d\d) \[([A-Za-z]+)\] ([^ ]+)`)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range flag.Args() {
+		for le := range LogLines(file, re) {
+			if class == "" || le.class == class {
+				if level == "" || le.level == level {
+					if searchRe == nil || searchRe.MatchString(le.line) {
+						fmt.Print(le.line)
+						if lines {
+							fmt.Println("--------------------------------------------------------------------------------")
+						}
+					}
+				}
+			}
 		}
 	}
 }
@@ -25,69 +65,31 @@ type LogEntry struct {
 	level     string
 }
 
-type LogReader struct {
-	path      string
-	re        *regexp.Regexp
-	lines     []string
-	current   int
-	nextEntry LogEntry
-}
-
-func newLogReader(path string, pattern string) LogReader {
-	re, err := regexp.Compile(pattern)
-	if err != nil {
-		panic(err)
-	}
-	lr := LogReader{
-		path:    path,
-		re:      re,
-		lines:   readFile(path),
-		current: 0,
-	}
-	for i, line := range lr.lines {
-		if match := re.FindStringSubmatch(line); match != nil {
-			lr.current = i
-			lr.nextEntry = LogEntry{
-				timeStamp: match[1],
-				class:     match[2],
-				level:     match[3],
-			}
-			break
-		}
-	}
-	return lr
-}
-
-func (lr *LogReader) next() *LogEntry {
-	if lr.current == len(lr.lines) {
-		return nil
-	}
-
-	le := lr.nextEntry
-
+func LogLines(path string, re *regexp.Regexp) func(func(LogEntry) bool) {
+	lines := readFile(path)
+	var nextEntry LogEntry
 	var sb strings.Builder
-	sb.WriteString(lr.lines[lr.current])
-	sb.WriteRune('\n')
 
-	for i := lr.current + 1; i < len(lr.lines); i++ {
-		if match := lr.re.FindStringSubmatch(lr.lines[i]); match != nil {
-			lr.current = i
-			lr.nextEntry = LogEntry{
-				timeStamp: match[1],
-				class:     match[2],
-				level:     match[3],
+	start, logEntry := findFirstEntry(lines, re)
+
+	return func(yield func(LogEntry) bool) {
+		for i := start + 1; i < len(lines); i++ {
+			if match := re.FindStringSubmatch(lines[i]); match != nil {
+				nextEntry = getEntry(match)
+
+				logEntry.line = sb.String()
+				if !yield(logEntry) {
+					return
+				}
+				logEntry = nextEntry
+				sb.Reset()
 			}
-			break
-		} else {
-			sb.WriteString(lr.lines[i])
+			sb.WriteString(lines[i])
 			sb.WriteRune('\n')
-			if i == len(lr.lines)-1 {
-				lr.current = len(lr.lines)
-			}
 		}
+		logEntry.line = sb.String()
+		yield(logEntry)
 	}
-	le.line = sb.String()
-	return &le
 }
 
 func readFile(path string) []string {
@@ -96,4 +98,22 @@ func readFile(path string) []string {
 		panic(err)
 	}
 	return strings.Split(string(b), "\n")
+}
+
+func findFirstEntry(lines []string, re *regexp.Regexp) (start int, logEntry LogEntry) {
+	for i, line := range lines {
+		if match := re.FindStringSubmatch(line); match != nil {
+			entry := getEntry(match)
+			return i, entry
+		}
+	}
+	return math.MaxInt - 1, LogEntry{}
+}
+
+func getEntry(match []string) LogEntry {
+	return LogEntry{
+		timeStamp: match[1],
+		level:     match[2],
+		class:     match[3],
+	}
 }
